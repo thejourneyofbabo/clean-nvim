59d592a931e57a23b281a824953522cbbc7603a7
thejourneyofbabo <=>
2024-10-03 13:48:44 +0900

    .

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,7 +65,7 @@ return {
   },
 
   {
-    'rcarriga/nvim-dap-ui', 
+    'rcarriga/nvim-dap-ui',
     dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
     config = function()
 			require("dapui").setup()
