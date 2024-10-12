0c0a2402c2727cb79ff6168af843dc8b3b79e14d
Jisang Yun <journeyofbabo@gmail.com>
2024-10-12 18:01:00 +0900

    change python set

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -120,7 +120,7 @@ return {
     config = function(_, opts)
       local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
       require("dap-python").setup(path)
-      require("core.utils").load_mappings("dap_python")
+      -- require("core.utils").load_mappings("dap_python")
     end,
   },
 
